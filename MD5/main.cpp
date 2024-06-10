cbf171b14776210c881e5e25e3bbc7eeab6c349f
hyuqing11 <yuiqinghuang2018@gmail.com>
2024-06-10 05:53:44 -0400

    Initial Commit

---
+++ MD5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MD5
+//
+//  Created by 黄宇青 on 6/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
